b4a1a73b160a1fd393e5f320211b6ee939eb325b
Fanta Coulibaly <fantacoulibaly101@gmail.com>
2024-07-25 00:41:14 +0000

    Fanta

--- script.js
+++ script.js
@@ -11,6 +11,25 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 
+  const swiper = new Swiper('.swiper-container', {
+    loop: true, // Boucle infinie
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true, // Permet de cliquer sur la pagination pour changer les slides
+      renderBullet: function (index, className) {
+        // Personnaliser le rendu des bullets de pagination
+        return `<div class="${className}" style="background-color: #d9d9d9; width: 41px; height: 5px; border-radius: 8px; margin: 0 5px;"></div>`;
+      },
+    },
+    autoplay: {
+      delay: 5000, // Délai entre les changements de slide (en millisecondes)
+    },
+    slidesPerView: 1, // Affiche une diapositive à la fois
+    spaceBetween: 50, // Espace entre les diapositives
+  });
+  
+  
+
   // Fait défiler la page vers le haut lorsque le bouton est cliqué
   scrollToTopBtn.addEventListener('click', () => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
